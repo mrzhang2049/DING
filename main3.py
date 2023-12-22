5c79b199b3183c5d29bd613a52ab79553721729f
hellozhang <hellozhangxf@qq.com>
2023-12-22 17:11:55 +0800

    2023-12-22 17:11:53

--- main3.py
+++ main3.py
@@ -70,9 +70,9 @@ if __name__ == '__main__':
     vika = Vika("uskKX37HkZuodf8VkY7CiQ1")
     # 通过 datasheetId 来指定要从哪张维格表操作数据。
     datasheet = vika.datasheet("dst1CSXS5xqdZJHTLZ", field_key="name")
-    i = 0
+    i = num
     session = requests.session()
-    while i <= num*8 and i>=(num-1)*8:
+    while i <= num*8 and i>(num-1)*8:
         i += 1
         headers = {
             'X-Forwarded-For': f'{random.randint(10, 126)}.{random.randint(10, 254)}.{random.randint(10, 254)}.{random.randint(10, 254)}'
