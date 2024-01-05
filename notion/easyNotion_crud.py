4bc49c5715fe7c6ebdba1246a2cb409d9b4643f7
张秀锋 <hellozhangxf@qq.com>
2024-01-05 13:55:14 +0800

    Revert "2024年1月5日13:51:39"
    This reverts commit 9f635cd82688af211de4e92c614ad8cccf03045b.

--- notion/easyNotion_crud.py
+++ notion/easyNotion_crud.py
@@ -1,3 +1,8 @@
+
+
+
+
+
 # easyNotion‼️使用限制
 # 只可用于notion中数据库类型为"Database - Full page"的数据库
 # 数据库中除title列其余所有列属性只能为text、ID、url
@@ -6,50 +11,32 @@
 
 from easyNotion import easyNotion
 from pprint import pprint
-from typing import List, Union
-
-from easyNotion.blocksModel import Divider, Mention, LinkPreview, RichText, Block
-
 db = easyNotion('c4db279645344510acb15d556caffce1', 'secret_j4748C1PwOII5JWcVb1Myn5Vqyw75cn6ggDtf2dBMYQ')
-dbPage = easyNotion('c4db279645344510acb15d556caffce1', 'secret_j4748C1PwOII5JWcVb1Myn5Vqyw75cn6ggDtf2dBMYQ',
-                    is_page=True)
 
 # 获取全部数据表
-# table = db.get_table()
-# col = db.get_table()
-# for itm in col:
-#     print(itm.__getitem__('Name'))
+table = db.get_table()
+col = db.query(['Name'])
+col = db.get_table()
+for itm in col:
+  print(itm.__getitem__('Name'))
 # 查询指定的列
-# col = db.query(['Filesmedia'], {'Name': '张三'})
+col = db.query(['Name'], {'Name': '张三'})
+pprint(col)
 
 # 插入新行
-# res = db.insert(
-#     {
-#         "Name": '新年贺词里的人民情怀',
-#         "Code": "12003",
-#         "CheckValue": True,
-#         "DAY": 'https://img2023.cnblogs.com/blog/35695/202312/35695-20231227181423297-1004649884.jpg',
-#         "WEEK": 'https://img2023.cnblogs.com/blog/35695/202311/35695-20231121143812081-926795020.png',
-#         "MONTH": 'https://gw.alipayobjects.com/zos/antfincdn/FLrTNDvlna/antv.png',
-#
-#     })
-content_blocks = [
-    LinkPreview(
-        url='https://example.com',
-        id='35695',
-        parent_id='4e3310016cb3427abdd362eaca7eec31'
-    ), LinkPreview(
-        url='https://example.com',
-        id='35695',
-        parent_id='4e3310016cb3427abdd362eaca7eec31'
-    )
-]
-dbPage.insert_page(content_blocks)
+res = db.insert({'Name': 'Qew','Filesmedia':'https://img.alicdn.com/tfs/TB13DzOjXP7gK0jSZFjXXc5aXXa-212-48.png'})
+pprint(res)
 
 # 更新指定的行
-res = db.update({'Name': 'new_value'}, {'Name': '张三'})
+res = db.update({'Name': 'new_value'},{'Name': '张三'})
 pprint(res)
 
 # 删除指定的行
 res = db.delete({'Name': 'new_value'})
 # pprint(res)
+
+
+
+
+
+
